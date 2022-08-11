a0f88f2c5d57edf8ac26633b1df37f7203c5eb9c
rektcherry <merituuli.jaaskelainen.com>
2022-08-11 22:12:18 +0300

    directory update

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/reset.css";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import Experience from "./Experience";
 
 ReactDOM.render(
-    <Router>
+    <Router basename="/webdeploy">
       <Routes>
     <Route path='/' element={<App/>} />
     <Route path='/experience' element={<Experience/>} />
