b22487b63c7294064443eb6f2b37205f5665a579
rektcherry <merituuli.jaaskelainen.com>
2022-08-11 22:13:13 +0300

    directory update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/reset.css";
-import {HashRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Experience from "./Experience";
 
 ReactDOM.render(
