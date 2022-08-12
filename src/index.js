8836709131fa465112d5193f3b10c4e4e3c1539d
rektcherry <merituuli.jaaskelainen.com>
2022-08-12 09:25:44 +0300

    routepathfix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import Experience from "./Experience";
 
 ReactDOM.render(
-    <Router baseline="/webdeploy">
+    <Router baseline="/">
     <Routes>
     <Route path='/' element={<App/>} />
     <Route path='/experience' element={<Experience/>} />
